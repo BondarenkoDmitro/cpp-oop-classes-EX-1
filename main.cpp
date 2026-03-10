6f2dce3975a9e1ed05686a91ac24dfda94ef8bb7
BondarenkoDmitro <kinglion2579@gmail.com>
2026-03-10 15:28:44 +0200

    Update 1: Show total objects count on creation

--- main.cpp
+++ main.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
-#include <conio.h>
+
 using namespace std;
-class Alpha{
+
+class Alpha {
 public:
     static int n;
     Alpha();
     ~Alpha();
 };
 int Alpha::n=0;
-Alpha::Alpha()
-{
+
+Alpha::Alpha() {
     if (n==0) cout<<"First object has been created"<<endl;
     n++;
 }
-Alpha::~Alpha()
-{
+Alpha::~Alpha() {
     n--;
     if (n==0) cout<<"Last element has been deleted";
 }
+
 int main() {
     Alpha *a;
     a=new Alpha[5];
     delete [] a;
-    getch();
 }
